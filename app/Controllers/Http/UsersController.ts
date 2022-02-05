aa8451d642d4a695863de3bc54065081a9ddb88a
uzzzzy <a.fauzy4b@gmail.com>
2022-02-05 17:39:16 +0800

    user controller

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,28 @@
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+
+export default class UsersController {
+  public async index({ request, response }) {
+    return response.json({
+      hello: "world",
+    });
+  }
+
+  public async create() {}
+
+  public async store({ request, response }) {
+    const { username, password } = request.only(["username", "password"]);
+
+    response.json({
+      username,
+      password,
+    });
+  }
+
+  public async show({}: HttpContextContract) {}
+
+  public async edit({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+}
